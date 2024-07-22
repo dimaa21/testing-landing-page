29a94f43ca98f91d95b486fbf4d07099acf5e17a
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-07-23 01:30:24 +0300

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,111 @@
 new Swiper('.image-slider', {
+  simulateTouch: true,
+
+  touchRatio: 1,
+
+  touchAngle: 45, 
+
+  grabCursor: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+    eventsTarget: '.image-slider',
+  },
+
+  autoHeight: true,
+
+  slidesPerView: 4.5,
+
+  spaceBetween: 10,
+
+  loop: true,
+
+  loopedSlised: 3,
+
+  freeMood: true,
+
+  autoplay: {
+    delay: 1000, 
+
+    stopOnLastSlide: true, 
+
+    disableOnInteraction: false,
+    
+    speed: 800
+    },
+    
+breakpoints: {
+    320: {
+        slidesPerView: 1,
+    },
+    480: {
+        slidesPerView: 2,
+    },
+    992: {
+        slidesPerView: 3,
+    }
+},
   
 });
+
+new Swiper('.image-slider-2', {
+    simulateTouch: true,
+  
+    touchRatio: 1,
+  
+    touchAngle: 45, 
+  
+    grabCursor: true,
+  
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    },
+  
+    mousewheel: {
+      sensitivity: 1,
+      eventsTarget: '.image-slider-2',
+    },
+  
+    autoHeight: true,
+  
+    slidesPerView: 4.5,
+  
+    spaceBetween: 5,
+  
+    loop: true,
+  
+    loopedSlised: 3,
+  
+    freeMood: true,
+  
+    autoplay: {
+      delay: 1000, 
+  
+      stopOnLastSlide: true, 
+  
+      disableOnInteraction: false,
+      
+      speed: 800
+      },
+      
+  breakpoints: {
+      320: {
+          slidesPerView: 1,
+      },
+      480: {
+          slidesPerView: 2,
+      },
+      992: {
+          slidesPerView: 3,
+      }
+  },
+    
+  });
